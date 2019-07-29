76c32a350538564f3f8a35c930a0c74d764ee14d
bwangelme <bwangel.me@gmail.com>
2019-07-29 23:30:23 +0800

    Feat: add highlight

--- markdown.go
+++ markdown.go
@@ -1,11 +1,52 @@
 package main
 
 import (
-	"github.com/shurcooL/github_flavored_markdown"
+	"bytes"
+
+	"github.com/russross/blackfriday"
 )
 
-func MarkDown(input []byte) []byte {
-	html := github_flavored_markdown.Markdown(input)
+const (
+	commonHtmlFlags = 0 |
+		blackfriday.HTML_USE_XHTML |
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_DASHES |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
 
-	return html
+	commonExtensions = 0 |
+		blackfriday.EXTENSION_NO_INTRA_EMPHASIS |
+		blackfriday.EXTENSION_TABLES |
+		blackfriday.EXTENSION_FENCED_CODE |
+		blackfriday.EXTENSION_AUTOLINK |
+		blackfriday.EXTENSION_STRIKETHROUGH |
+		blackfriday.EXTENSION_SPACE_HEADERS |
+		blackfriday.EXTENSION_HEADER_IDS |
+		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
+		blackfriday.EXTENSION_DEFINITION_LISTS
+)
+
+type Renderer struct {
+	*blackfriday.Html
+}
+
+func (r *Renderer) ListItem(out *bytes.Buffer, text []byte, flags int) {
+	switch {
+	case bytes.HasPrefix(text, []byte("[ ] ")):
+		text = append([]byte(`<input type="checkbox" disabled="">`), text[3:]...)
+	case bytes.HasPrefix(text, []byte("[x] ")) || bytes.HasPrefix(text, []byte("[X] ")):
+		text = append([]byte(`<input type="checkbox" checked="" disabled="">`), text[3:]...)
+	}
+	r.Html.ListItem(out, text, flags)
+}
+
+func NewRenderer() *Renderer {
+	return &Renderer{
+		Html: blackfriday.HtmlRenderer(commonHtmlFlags, "", "").(*blackfriday.Html),
+	}
+}
+
+func MarkDown(input []byte) []byte {
+	render := NewRenderer()
+	return blackfriday.Markdown(input, render, commonExtensions)
 }
